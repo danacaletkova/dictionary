2cb843c51de4bb1b21960d400593ee809a1c312e
danacaletkova <164472411+danacaletkova@users.noreply.github.com>
2024-05-21 14:42:29 +0800

    Make design responsive

--- src/Gallery.js
+++ src/Gallery.js
@@ -7,7 +7,7 @@ export default function Gallery(props) {
         <div className="row">
           {props.photos.map(function (photo, index) {
             return (
-              <div className="col-4 p-1" key={index}>
+              <div className="col-6 col-sm-4 p-1" key={index}>
                 <a href={photo.src.original} target="_blank" rel="noreferrer">
                   <img
                     src={photo.src.landscape}
