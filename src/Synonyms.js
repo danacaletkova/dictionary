222743baa8c3f0018565d6b2812b05e3b9ee9cde
danacaletkova <164472411+danacaletkova@users.noreply.github.com>
2024-05-21 12:59:20 +0800

    Change colors and font sizes

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -4,7 +4,7 @@ import "./Synonyms.css";
 export default function Synonyms(props) {
   if (props.synonyms) {
     return (
-      <div className="Synonyms">
+      <div className="Synonyms mt-3">
         {props.synonyms.map((synonym, index) => {
           return (
             <div key={index} className="synonym">
