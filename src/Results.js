a29f9ee92797cf7d9059397d43f20e110702da95
danacaletkova <164472411+danacaletkova@users.noreply.github.com>
2024-05-21 14:30:26 +0800

    Handle missing phonetics

--- src/Results.js
+++ src/Results.js
@@ -3,11 +3,19 @@ import Meaning from "./Meaning";
 import "./Results.css";
 
 export default function Results(props) {
+  function formatPhonetic(phonetic) {
+    if (phonetic) {
+      return <h3>/{phonetic}/</h3>;
+    } else {
+      return <h3>{""}</h3>;
+    }
+  }
+
   if (props.results) {
     return (
       <div className="Results">
         <h2>{props.results.word}</h2>
-        <h3>/{props.results.phonetic}/</h3>
+        {formatPhonetic(props.results.phonetic)}
         {props.results.meanings.map((meaning, index) => {
           return (
             <section key={index}>
